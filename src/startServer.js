ccba836120cb8dff794edd85aa096fe7f87c951f
Silas Sampaio Gomide Rosa <51147811+gomideDev@users.noreply.github.com>
2021-02-22 16:26:43 -0300

    add port

--- src/startServer.js
+++ src/startServer.js
@@ -24,8 +24,7 @@ function startServer({typeDefs, resolvers}){
     const pubsub = new PubSub();
 
     const server = new ApolloServer({typeDefs, resolvers, context: {pubsub}});
-
-    server.listen().then(({url})=>console.log(`Server started at ${url}`));
+    server.listen({port: process.env.PORT || 4000}).then(({url})=>console.log(`Server started at ${url}`));
 }
 
 export default startServer;
